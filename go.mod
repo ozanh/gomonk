f2eb347c6e472de3ff5d00f6761d69f4385f568f
Kevser <kevser.sirca@binalyze.com>
2022-06-16 23:57:42 +0300

    refactor(cli): added cli and help command

---
+++ go.mod
@@ -0,0 +1,18 @@
+module gomonk
+
+go 1.18
+
+require github.com/urfave/cli/v2 v2.8.1
+
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/stretchr/testify v1.7.2
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+
+	"gomonk/cmd/help"
+)
+
+var Version = "v0.1.0"
+
+func main() {
+	app := &cli.App{
+		Name:     "gomonk",
+		Usage:    "Do monitoring with extreme skill.",
+		Commands: Commands(os.Stdin),
+		Version:  Version,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Commands(reader io.Reader) []*cli.Command {
+	return []*cli.Command{
+		help.Command(),
+	}
+}
